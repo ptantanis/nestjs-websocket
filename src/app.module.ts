3ae512fe717b3f83a14a9c30f426ed0553f6bc57
ptantanis <p.tantanis@live.com>
2023-09-10 23:28:00 +0700

    add 2 events gateway

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Event1Gateway } from './event1.gateway';
+import { Event2Gateway } from './event2.gateway';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, Event1Gateway, Event2Gateway],
 })
 export class AppModule {}
